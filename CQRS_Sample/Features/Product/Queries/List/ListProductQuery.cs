c92cc5915f7d2693b32d655edd36ef4e5b0d9d1f
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2025-03-07 15:13:13 +0100

    feat: created the product query and product query handler.

---
+++ CQRS_Sample/Features/Product/Queries/List/ListProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using CQRS_Sample.Features.Product.Dtos;
+using MediatR;
+
+namespace CQRS_Sample.Features.Product.Queries.List
+{
+    public record ListProductQuery : IRequest<List<ProductDto>>
+    {
+    }
+}
---
+++ CQRS_Sample/Features/Product/Queries/List/ListProductQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using CQRS_Sample.Features.Product.Dtos;
+using CQRS_Sample.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Sample.Features.Product.Queries.List
+{
+    public class ListProductQueryHandler(AppDbContext context) : IRequestHandler<ListProductQuery, List<ProductDto>>
+    {
+        public async Task<List<ProductDto>> Handle(ListProductQuery request, CancellationToken cancellationToken)
+        {
+            return await context.Products
+                .Select(p => new ProductDto(p.Id, p.Name, p.Description, p.Price))
+                .ToListAsync();
+        }
+    }
+}
