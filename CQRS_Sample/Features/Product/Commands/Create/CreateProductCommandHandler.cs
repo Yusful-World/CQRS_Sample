3221376d198e006d8e24ed81a9e653da9eb5c485
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2025-03-07 22:04:53 +0100

    feat: Added delete command and handler.

---
+++ CQRS_Sample/Features/Product/Commands/Create/CreateProductCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using CQRS_Sample.Domain;
+using CQRS_Sample.Persistence;
+using MediatR;
+
+namespace CQRS_Sample.Features.Product.Commands.Create
+{
+    public class CreateProductCommandHandler(AppDbContext context)
+        : IRequestHandler<CreateProductCommand, Guid>
+    {
+        public async Task<Guid> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Domain.Product(command.Name, command.Description, command.Price);
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
+
+            return product.Id;
+        }
+    }
+}
---
+++ CQRS_Sample/Features/Product/Commands/Delete/DeleteProductCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace CQRS_Sample.Features.Product.Commands.Delete
+{
+    public record DeleteProductCommand(Guid Id) : IRequest
+    {
+    }
+}
