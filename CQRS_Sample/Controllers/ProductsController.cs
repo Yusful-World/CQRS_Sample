516db07908ab0a2ae8cd8de06d757b87860b3902
Abdullahi Yusuf <abdullahiyusuf042000@gmail.com>
2025-03-09 14:58:02 +0100

    feat: Added endpoints for all crud operations.

---
+++ CQRS_Sample/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using CQRS_Sample.Features.Product.Commands.Create;
+using CQRS_Sample.Features.Product.Commands.Delete;
+using CQRS_Sample.Features.Product.Commands.Update;
+using CQRS_Sample.Features.Product.Queries.Get;
+using CQRS_Sample.Features.Product.Queries.List;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_Sample.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController(ISender sender) : ControllerBase 
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetProductList()
+        {
+            var productList = await sender.Send(new ListProductQuery());
+            
+            return Ok(productList);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            var product = await sender.Send(new GetProductQuery(id));
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductCommand createProductCommand)
+        {
+            var productId = await sender.Send(createProductCommand);
+            if (Guid.Empty == productId)
+            {
+                return BadRequest("Error creating product");
+            }
+
+            return CreatedAtAction(nameof(GetProduct), productId);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateProduct(Guid id, UpdateProductCommand updateProductCommand)
+        {
+            if (id != updateProductCommand.Id)
+                return BadRequest("product Id mismatch");
+
+            var updatedProduct = await sender.Send(updateProductCommand);
+            
+            return updatedProduct != null ? Ok(updatedProduct) : NotFound();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            await sender.Send(new DeleteProductCommand(id));
+            if (Guid.Empty == id)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
